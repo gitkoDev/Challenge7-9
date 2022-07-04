459fb7d8e1f73b316c2c7512d3f6280a515e6cb1
gitkoDev <prostodenis1397@gmail.com>
2022-07-04 12:08:16 +0800

    Initial Commit

---
+++ Challenge7-9/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Challenge7-9
+//
+//  Created by Gitko Denis on 04.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
